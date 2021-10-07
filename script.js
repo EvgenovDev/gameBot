7d732b2e4044390ca36d40d6d649b6782fe7948c
Roman Evgenov <Roman Evgenov>
2021-10-07 18:53:00 +0300

    Доработал

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const isNumber = (num) => {
 
 const startGame = () => {
 	alert(`Вас приветствует игра "Загадывание случайного числа от 1 до 100!`);
+
 	let ourNumber = 23;
-	let mistake = 4;
+	let mistake = 10;
+
 	const repeatGame = () => {
 		let num = prompt("Число ваших попыток = " + mistake + " Введите ваше число");
 			if (num == ourNumber && isNumber(num)) {
-				if(prompt("Поздравляю, Вы угадали!!! Хотите сыграть еще?","Нажмите ОК,чтобы сыграrepeatGameз или отмена чтобы закрыть игру") == "string") {
-					mistake = 10;
-					repeatGame();
-				} else if (num === null) {
-					return alert("Игра окончена");
-				}
+				num = prompt("Поздравляю, Вы угадали!!! Хотите сыграть еще?","Нажмите ОК,чтобы сыграть еще раз или отмена чтобы закрыть игру");
+					if(typeof(num) === "string") {
+						startGame();
+					} else if (num === null) {
+						return alert("Игра окончена");
+					}
 			} else if (num < ourNumber && isNumber(num)) {
 				if(mistake > 1){
 					--mistake;
@@ -25,9 +27,8 @@ const startGame = () => {
 					repeatGame();
 				} else {
 						num = prompt("Попытки закончились хотите сыграть еще?","Нажмите ОК,чтобы сыграть еще раз или отмена чтобы закрыть игру");
-							if(num == "string") {
-								mistake = 10;
-								repeatGame();
+							if(typeof(num) === "string") {
+								startGame();
 							} else if (num === null) {
 								return alert("Игра окончена");
 							}
@@ -39,9 +40,8 @@ const startGame = () => {
 					repeatGame();
 				} else {
 						num = prompt("Попытки закончились хотите сыграть еще?","Нажмите ОК,чтобы сыграть еще раз или отмена чтобы закрыть игру");
-							if(num == "string") {
-								mistake = 10;
-								repeatGame();
+							if(typeof(num) === "string") {
+								startGame();
 							} else if (num === null) {
 								return alert("Игра окончена");
 							}
@@ -55,5 +55,6 @@ const startGame = () => {
 
 		repeatGame();
 };
+
 
 startGame();
